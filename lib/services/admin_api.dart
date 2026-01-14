d2c22dc52fd8158a47dbaf83ecb47438052de0eb
Lakshmanan Palaniappan <154072727+Lakshmanan-palaniappan@users.noreply.github.com>
2026-01-14 14:03:39 +0530

    link

--- lib/services/admin_api.dart
+++ lib/services/admin_api.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-const String backendBaseUrl = "https://cbbc7d827604.ngrok-free.app";
+const String backendBaseUrl = "https://3acfa652cb4f.ngrok-free.app";
 
 class AdminApi {
   static Map<String, String> headers = {
